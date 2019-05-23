cb97ac57e5212ee609ec0b9f41403e2ffb69f5ab
Michael <3yebmb@gmail.com>
2019-05-23 10:43:32 +0500

    add data binding class

--- app/src/main/java/dev/m13d/aboutme/MainActivity.kt
+++ app/src/main/java/dev/m13d/aboutme/MainActivity.kt
@@ -7,14 +7,18 @@ import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.databinding.DataBindingUtil
 import dev.m13d.aboutme.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val myName: MyName = MyName("Michael 3yebmb")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.myName = myName
 
         binding.doneButton.setOnClickListener {
             addNickname(it)
@@ -24,6 +28,7 @@ class MainActivity : AppCompatActivity() {
     private fun addNickname(view: View) {
         binding.apply {
             binding.nicknameText.text = binding.nicknameEdit.text
+            myName?.nickname = nicknameEdit.text.toString()
             invalidateAll()
             binding.nicknameEdit.visibility = View.GONE
             binding.doneButton.visibility = View.GONE
---
+++ app/src/main/java/dev/m13d/aboutme/MyName.kt
@@ -0,0 +1,3 @@
+package dev.m13d.aboutme
+
+data class MyName(var name: String = "", var nickname: String = "")
